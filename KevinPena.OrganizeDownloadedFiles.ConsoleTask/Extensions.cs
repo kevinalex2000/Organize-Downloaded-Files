c820ea246e1ecde5096392f79fba5b10eb0ff40e
kevinalexdev <kevinalexpe@gmail.com>
2022-08-06 14:41:47 -0500

    first commit

---
+++ KevinPena.OrganizeDownloadedFiles.ConsoleTask/Extensions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KevinPena.OrganizeDownloadedFiles.ConsoleTask
+{
+    public class Extensions
+    {
+        public static string[] pictures = { ".jpeg", ".jpg", ".png" };
+        public static string[] documents = { ".xlsx", ".pdf", ".docx" };
+    }
+}
---
+++ KevinPena.OrganizeDownloadedFiles.ConsoleTask/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using KevinPena.OrganizeDownloadedFiles.ConsoleTask;
+
+string downloadFolder = @"D:\Users\Equipo\Downloads";
+
+DirectoryInfo dirDownload = new DirectoryInfo(downloadFolder);
+
+foreach (var file in dirDownload.GetFiles())
+{
+    //To Pictures
+    if (Extensions.pictures.Contains(file.Extension))
+    {
+        file.MoveTo(Environment.GetFolderPath(Environment.SpecialFolder.MyPictures)+@"\"+file.Name);
+    }
+    //To Documents
+    if (Extensions.documents.Contains(file.Extension))
+    {
+        file.MoveTo(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + @"\" + file.Name);
+    }
+}
